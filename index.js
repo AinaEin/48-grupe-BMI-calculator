883f519013196609af21e64155ed7343d0f8d13c
Aina <ainaeinikyte@gmail.com>
2024-03-04 08:34:26 +0200

    Update

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-function Calculate() {
+function calculate() {
   const height = document.getElementById("Height").value;
   const weight = document.getElementById("Weight").value;
-  const clearBtn = document.getElementById("clearBtn");
-  clearBtn.classList.add("active");
-  
-  if (height && weight) {
-    clearBtn.disabled = false;
-  } else {
-    clearBtn.disabled = true;
-  }
+  const output = document.getElementById("output");
 
   let bmi = weight / (height / 100) ** 2;
   bmi = bmi.toFixed(2);
 
   if (bmi > 0 && bmi <= 18.5) {
-    document.getElementById("output").innerHTML = "Your BMI is : <b>" + bmi;
-    output.style.color = "Gold";
+    output.innerHTML = 'Your BMI is : <b>' + bmi + '</b><br>Your weight is <span>Underweight</span>.';
+    output.style.color = "gold";
   } else if (bmi > 18.5 && bmi <= 25) {
-    document.getElementById("output").innerHTML = "Your BMI is : <b>" + bmi;
+    output.innerHTML = "Your BMI is : <b>" + bmi + "</b><br>Your weight is <span>Normal (healthy weight)</span>.";
     output.style.color = "green";
   } else if (bmi > 25 && bmi <= 40) {
-    document.getElementById("output").innerHTML = "Your BMI is : <b>" + bmi;
+    output.innerHTML = "Your BMI is : <b>" + bmi + "</b><br>Your weight is <span>Overweight</span>.";
     output.style.color = "red";
   } else {
-    document.getElementById("output").innerHTML = "Please type valid numbers.";
+    output.innerHTML = "Please type valid numbers.";
   }
 }
 
-/*const isDisabled = () => {
+const isDisabled = () => {
     
   const height = document.getElementById("Height").value;
   const weight = document.getElementById("Weight").value;
@@ -46,15 +39,17 @@ function Calculate() {
     }
     
     
-};*/
+};
 
-const clearBtn = document.getElementById("clearBtn");
+
+const clearBtn = document.getElementById('clearBtn')
 function clear() {
-  document.getElementById("Height").value = null;
-  document.getElementById("Weight").value = null;
 
-  clearBtn.classList.remove("active");
-  document.getElementById("output").innerHTML = "";
+    document.getElementById("Height").value = null;
+    document.getElementById("Weight").value = null;
+    
+    clearBtn.classList.remove('active');
+    document.getElementById("output").innerHTML = "";
 }
-
-clearBtn.addEventListener("click", clear);
+ 
+clearBtn.addEventListener('click', clear);
